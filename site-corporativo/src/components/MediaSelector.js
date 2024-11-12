02d4368784b8950a84ef90c7b9877331882ee1b9
Daniel-Celtica <85639347+Daniel-Celtica@users.noreply.github.com>
2024-11-12 15:32:22 -0300

    fix: mediaSelector em telas menores

--- site-corporativo/src/components/MediaSelector.js
+++ site-corporativo/src/components/MediaSelector.js
@@ -41,7 +41,7 @@ const MediaSelector = ({ media }) => {
     }
 
     return (
-        <div ref={mediaContainerRef} className={`relative -mx-16 lg:-mx-64 mt-10 ${expanded ? "pb-[760px]" : ""}`}>
+        <div ref={mediaContainerRef} className={`relative -mx-8 lg:-mx-64 mt-10 ${expanded ? "pb-[760px]" : ""}`}>
             <div className="relative overflow-visible">
                 {/* Container para os slides */}
                 <div
